156b176ae37622987353128a4c294fb23d48810c
Seoin <seoin2269@naver.com>
2024-12-17 13:49:13 +0900

    feat: CookieProvider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import router from './app/routes';
+import { CookiesProvider } from 'react-cookie';
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <CookiesProvider>
+      <RouterProvider router={router} />;
+    </CookiesProvider>
+  );
 }
 
 export default App;
