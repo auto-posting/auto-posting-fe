9cdd5dfb183d4a60306bd4457188a8f84ec68d00
Seoin <seoin2269@naver.com>
2025-02-04 13:03:38 +0900

    style: nav-link 공통 스타일 클래스 지정

--- src/app/ui/Layout/Header.tsx
+++ src/app/ui/Layout/Header.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import menuBar from '@/assets/svgs/bars.svg';
 import SideMenu from '../SideMenu';
 import useSideMenu from '@/shared/model/useSideMenu';
@@ -7,18 +8,35 @@ import useAuth from '@/features/auth/model/useAuth';
 export default function Header() {
   const { triggerRef, handleSideMenuOpen, isSideMenuOpen } = useSideMenu();
   const { handleLogout } = useAuth();
+  const [activeLink, setActiveLink] = useState<string | null>('/my');
+
+  const handleLinkClick = (link: string) => {
+    setActiveLink(link);
+  };
 
   return (
     <header className="w-full sticky top-0 flex items-center justify-between border-b border-sub px-1 py-2">
       <h2 className="text-main">AUTOPO</h2>
       <nav className="flex items-center gap-5">
-        <Link to="/my" className="sm:text-sub sm:mr-5 sm:font-bold sm:block hidden">
+        <Link
+          to="/my"
+          className={`${activeLink === '/my' ? 'sm:text-main ' : ''}nav-link`}
+          onClick={() => handleLinkClick('/my')}>
           내 정보
         </Link>
-        <Link to="/setting" className="sm:text-sub sm:mr-5 sm:font-bold sm:block hidden">
+        <Link
+          to="/setting"
+          className={`${activeLink === '/setting' ? 'sm:text-main ' : ''}nav-link`}
+          onClick={() => handleLinkClick('/setting')}>
           포스팅 설정
         </Link>
-        <Link to="/login" onClick={handleLogout} className="sm:text-sub sm:mr-5 sm:font-bold sm:block hidden">
+        <Link
+          to="/login"
+          className="nav-link"
+          onClick={() => {
+            handleLinkClick('/login');
+            handleLogout();
+          }}>
           로그아웃
         </Link>
         <button className="sm:hidden" onClick={handleSideMenuOpen} ref={triggerRef}>
